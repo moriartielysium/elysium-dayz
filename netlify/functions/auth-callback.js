c5016cd131c5f58387ddefe7c76b05caa38f9983
moriartielysium <moriartielysium@gmail.com>
2026-03-17 21:04:28 +0300

    Update auth-callback.js

--- netlify/functions/auth-callback.js
+++ netlify/functions/auth-callback.js
@@ -1,21 +1,28 @@
 const { exchangeCodeForToken, getCurrentUser } = require("./_lib/discord");
 const { redirect } = require("./_lib/response");
-const { createSession, getStateFromCookies, clearStateCookie } = require("./_lib/session");
+const { createSession, getStateFromCookies } = require("./_lib/session");
 const { getConfig } = require("./_lib/env");
+
 const config = getConfig();
+
 exports.handler = async (event) => {
   try {
-    const params = new URLSearchParams(event.rawQuery || "");
+    const params = new URLSearchParams(event.queryStringParameters || {});
     const code = params.get("code");
     const state = params.get("state");
+
     const savedState = getStateFromCookies(event);
+
     if (!code || !state || !savedState || state !== savedState) {
-      return redirect(`${config.siteUrl}/?error=oauth_state`, [clearStateCookie()]);
+      return redirect(`${config.siteUrl}/?error=oauth_state`);
     }
+
     const tokenData = await exchangeCodeForToken(code);
     const user = await getCurrentUser(tokenData.access_token);
+
     const sessionCookie = await createSession(user, tokenData);
-    return redirect(`${config.siteUrl}/dashboard.html`, [clearStateCookie(), sessionCookie]);
+
+    return redirect(`${config.siteUrl}/dashboard.html`, [sessionCookie]);
   } catch (error) {
     console.error("auth-callback error:", error);
     return redirect(`${config.siteUrl}/?error=oauth_callback`);
