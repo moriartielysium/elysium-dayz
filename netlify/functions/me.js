0e85cef21abd6deaf9e68ec1ccd21d23aa193e59
moriartielysium <moriartielysium@gmail.com>
2026-03-17 21:55:30 +0300

    Update me.js

--- netlify/functions/me.js
+++ netlify/functions/me.js
@@ -1,10 +1,21 @@
 const { getSession } = require("./_lib/session");
 const { ok, unauthorized, serverError } = require("./_lib/response");
+const { isSuperAdmin } = require("./_lib/admin");
+
 exports.handler = async (event) => {
   try {
     const session = await getSession(event);
-    if (!session) return unauthorized("Not logged in");
-    return ok({ ok: true, user: session.user });
+    if (!session) {
+      return unauthorized("Not logged in");
+    }
+
+    return ok({
+      ok: true,
+      user: {
+        ...session.user,
+        isSuperAdmin: isSuperAdmin(session.user.id)
+      }
+    });
   } catch (error) {
     console.error("me error:", error);
     return serverError();
