92e0ec3b5348895bc2dbd92c46d111c782945f7d
moriartielysium <moriartielysium@gmail.com>
2026-04-11 12:45:27 +0300

    Delete netlify/functions directory

--- netlify/functions/_lib/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-const { getConfig } = require("./env");
-const config = getConfig();
-const pool = new Pool({
-  connectionString: config.databaseUrl,
-  ssl: { rejectUnauthorized: false }
-});
-async function query(text, params = []) { return pool.query(text, params); }
-module.exports = { query };
--- netlify/functions/auth-logout.js
+++
@@ -1,9 +0,0 @@
-const { clearSessionCookie } = require("./_lib/session");
-const { redirect } = require("./_lib/response");
-const { getConfig } = require("./_lib/env");
-
-const config = getConfig();
-
-exports.handler = async () => {
-  return redirect(`${config.siteUrl}/`, [clearSessionCookie()]);
-};
