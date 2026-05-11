a1f0fa83eceda44eac534c90f08e17796828a6e9
moriartielysium <moriartielysium@gmail.com>
2026-05-12 00:37:14 +0300

    654

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@ window.ELYSIUM_CONFIG = {
   domain: 'https://elysium-dayz.site',
   discordUrl: 'https://discord.gg/elysium-dayz',
   supportEmail: 'elysiumdayz.support@gmail.com',
-  sellerName: 'Самозанятый Оськин А.А.',
+  sellerName: 'Самозанятый Оськин Александр Александрович',
   sellerInn: '770172458319',
   sellerCity: 'Москва'
 };
